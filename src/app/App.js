2334a58a2d8966d3dad79608de64616dfd7e50f9
Alexey Nikulin <researcher190b@gmail.com>
2022-12-04 11:31:52 +0300

    start diplom

---
+++ src/app/App.js
@@ -0,0 +1,15 @@
+import NavBar from "./components/ui/navbar";
+
+// import logo from './logo.svg';
+// import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <h1>hello world</h1>
+    </div>
+  );
+}
+
+export default App;
